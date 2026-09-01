da261dfe397e56b344c2daa223483c4e3d3ebfa8
Junheng-Zhu <116934826+Junheng-Zhu@users.noreply.github.com>
2026-09-01 14:44:09 +0800

    tool registry

--- src/tools/registry.py
+++ src/tools/registry.py
@@ -2,18 +2,19 @@ from base import Tool
 
 class ToolRegistry:
 
-    _tool={}
+
 
 
     def __init__(self):
+        self._tools={}
 
 
     def register(self, tool:Tool ):
 
-        self._tool[tool.name]=tool
-#z这里为什么报错，应为缩进块
-    def list_tool(self):
-        return self._tool.keys()
+        self._tools[tool.name]=tool
+
+    def list_tools(self):
+        return self._tools.keys()
         
     
     def get(self,name:str)->Tool:
