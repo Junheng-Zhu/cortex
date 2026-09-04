903a403b9f8325f59e042a13d6ff2bfb6b7cd5ad
Junheng-Zhu <116934826+Junheng-Zhu@users.noreply.github.com>
2026-09-04 11:27:30 +0800

    schemas

---
+++ playground/test_schemas.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+# 获取项目根目录：playground 的上一级
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, PROJECT_ROOT)
+
+
+from src.tools.schemas import ReadNoteInput
+
+# tool_Read=ReadNoteInput()
+# tool_Read(path = "notes/python.md")
+# tool_Read(path = 123)
+
+tool_read = ReadNoteInput(path="notes/python.md")
+
+print(tool_read)
+print(tool_read.path)
+print(type(tool_read))
+print(ReadNoteInput.model_json_schema())
+
+
+ReadNoteInput()
+print(ReadNoteInput.model_json_schema())
+
+ReadNoteInput(path=123)
+print(ReadNoteInput.model_json_schema())
